e9ec90f61e96892d3ef41a567892150ab1f04320
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-04-06 20:57:47 -0400

    durable add to queue

--- app/lib/mb/broker.py
+++ app/lib/mb/broker.py
@@ -21,13 +21,13 @@ class RabbitMQBroker:
         self.channel = self.connection.channel()
 
     def publish(self, queue_name: str, message: str):
-        self.channel.queue_declare(queue=queue_name, durable=False)
+        self.channel.queue_declare(queue=queue_name, durable=True)
         self.channel.basic_publish(
             exchange='',
             routing_key=queue_name,
             body=message,
             properties=pika.BasicProperties(
-                delivery_mode=2,  # make message persistent
+                delivery_mode=1,
             ),
         )
 
